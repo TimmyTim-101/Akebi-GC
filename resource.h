d53bfc1f0684d4aabe81811f442c0d5a287b13ea
avottt <129268331+avottt@users.noreply.github.com>
2023-04-14 20:27:16 +0200

    Add files via upload

--- resource.h
+++ resource.h
@@ -1,15 +1,21 @@
-﻿//{{NO_DEPENDENCIES}}
+//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by injector.rc
+// Used by res.rc
 //
-#define IDI_ICON1                       101
-#define IDB_PNG1                        102
+#define AssemblyChecksum                4
+#define IDR_RCDATA1                     101
+#define IDR_RCDATA2                     102
+#define IDB_PNG1                        105
+#define IDB_PNG2                        106
+#define IDB_ANEMOCULUS                  106
+#define IDR_RCDATA3                     107
+#define IDR_RCDATA4                     110
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
